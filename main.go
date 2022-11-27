bdbdd4f1f52aae1f01d2c279d7635688ac1d8238
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-11-27 12:57:30 +0330

    finish

--- main.go
+++ main.go
@@ -106,3 +106,60 @@ func Bank(w http.ResponseWriter, r *http.Request) {
 
 	// 302 is an arbitrary number chosen by the developer to retrieve data
 }
+
+func CallBack(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "برگشت از درگاه")
+	// In localhost, there are two functional parameters, one is authority, which is the payment identifier,
+	// and the other is statuscode, which shows ok for successful payment and shows nook or nok for unsuccessful payment.
+	// the parameters that we have to recieve
+	authority := r.URL.Query().Get("Authority")
+	status := r.URL.Query().Get("Status")
+
+	// If none of these are present, it will automatically return an empty string
+	if authority == "" || status == "" || status != "OK" {
+		fmt.Fprintln(w, "خطایی در پرداخت رخ داده است")
+		w.WriteHeader(http.StatusOK)
+		// StatusOk It means that the connection to the server was made, but an error occurred
+		return
+	}
+
+	vars := mux.Vars(r)
+	price, ok := vars["price"]
+	if !ok {
+		fmt.Fprintln(w, "لطفا مبلغ را وارد کنید.")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	intPrice, err := strconv.Atoi(price)
+	if err != nil {
+		fmt.Fprintln(w, "لطفا مبلغ را بصورت عدد وارد کنید.")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// To use the price after copying the above, we need to reconnect to Zarin Pal
+	zarinpal, err := zarinpal.NewZarinpal(MERCHAND_ID, SANDBOX)
+	if err != nil {
+		fmt.Fprintln(w, "خطا در پرداخت.")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	verified, refId, statusCode, err := zarinpal.PaymentVerification(intPrice, authority)
+	if err != nil {
+		if statusCode == 101 {
+			fmt.Fprintln(w, "این پرداخت موفق بوده و قبلا این عملیات انجام شده است.")
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		fmt.Fprintln(w, "خطا در پرداخت.")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	// 101 is an arbitrary number chosen by the developer, which was created to maintain the security of the site so that the user does not perform the payment twice after being verified and creating the status of the operation code.
+	fmt.Fprintln(w, "پرداخت موفقیت آمیز بود . شماره پیگیری : ", refId)
+
+	//Create Transaction in DB
+	fmt.Println(w, "Payment Verified : ", verified, " ,  refId: ", refId, " statusCode: ", statusCode)
+}
